a77bae4a9a7e6565bf7269473f19cdbb4cccea88
tracer-ics2024 <tracer@njuics.org>
2026-02-14 05:11:02 +0000

    >  compile NEMU 000000 TEMP Linux 5b037ab48c24 6.10.14-linuxkit #1 SMP Tue Oct 14 07:32:13 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  05:11:02 up 1 day,  2:01,  0 users,  load average: 0.08, 0.14, 0.16

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -10,11 +10,69 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  char *p = out;
+  int n = 0;
+  
+  for (const char *f = fmt; *f; f++) {
+    if (*f != '%') {
+      *p++ = *f;
+      n++;
+      continue;
+    }
+
+    f++;
+    switch (*f) {
+      case 's':
+        {
+          const char *s = va_arg(ap, const char *);
+          if (!s) s = "(null)";
+          while (*s) {
+            *p++ = *s++;
+            n++;
+          }
+          break;
+        }
+      case 'd':
+        {
+          int v = va_arg(ap, int);
+          unsigned int u;
+          if (v < 0) {
+            *p++ = '-';
+            n++;
+            u = (unsigned int)(- (unsigned int)v);
+          } else {
+            u = (unsigned int)v;
+          }
+          
+          char tmp[16];
+          int k = 0;
+          do {
+            tmp[k++] = '0' + (u % 10);
+            u /= 10;
+          } while (u);
+          while(k--) {
+            *p++ = tmp[k];
+            n++;
+          }
+          break;
+        }
+      default:
+        if (*f) {
+          *p++ = *f;
+          n++;
+        }
+    }
+  }
+  *p = '\0';
+  return n;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list ap;
+  va_start(ap, fmt);
+  int ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
